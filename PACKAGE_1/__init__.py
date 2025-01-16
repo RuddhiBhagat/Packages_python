bea5c52c6e9430f5993117da0a39b3a637279b9f
RuddhiBhagat <ruddhi.bhagat@atyeti.com>
2025-01-16 15:57:46 +0530

    Adding packages

---
+++ PACKAGE_1/__init__.py
@@ -0,0 +1,4 @@
+# This can be used to initialize your package
+from .logger import setup_logger
+
+logger = setup_logger()
---
+++ main.py
@@ -0,0 +1,5 @@
+from PACKAGE_1 import logger as logger1
+from PACKAGE_2 import logger as logger2
+
+logger1.debug("This is a debug message from mypackage1.")
+logger2.info("This is an info message from mypackage2.")
